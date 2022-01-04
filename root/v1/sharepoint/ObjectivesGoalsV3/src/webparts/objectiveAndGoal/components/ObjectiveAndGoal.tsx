abf82603c0c8d738bedac3e4fd57205694813f33
mangeshbhunde <69294754+mangeshbhunde@users.noreply.github.com>
2022-01-04 15:08:03 +0530

    Updated ObjectiveAndGoal.tsx file
    Updated ObjectiveAndGoal.tsx file

--- root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/ObjectiveAndGoal.tsx
+++ root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/ObjectiveAndGoal.tsx
@@ -27,12 +27,9 @@ export default class ObjectiveAndGoal extends React.Component<IObjectiveAndGoalP
           <div className={ styles.container }>
             <div className={ styles.row }>
               <div className={ styles.column }>
-                <span className={ styles.title }>Welcome to SharePoint!</span>
-                <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-                <p className={ styles.description }>{escape(this.props.description)}</p>
-                <a href="https://aka.ms/spfx" className={ styles.button }>
-                  <span className={ styles.label }>Learn more</span>
-                </a>
+                <span className={ styles.title }>Welcome to React-Redux</span>
+                <p className={ styles.subTitle }>Implementation Redux.</p>
+                <p className={ styles.description }>{escape(this.props.description)}</p>               
                 <ChildComponent1 />
               </div>
             </div>
