a77d349c0c83e8684f912066b7dc0f49eac01f19
Sourabh <77320234+SourabhMK@users.noreply.github.com>
2022-01-07 14:46:07 +0530

    Changes for appData.sectors to be fetched from store

--- root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabHeader.tsx
+++ root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabHeader.tsx
@@ -1,24 +1,30 @@
 import * as React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from './TabHeader.module.scss';
 import { ITabHeaderProps } from './ITabHeaderProps';
 import { connect } from "react-redux";
 import TabItem from "./TabItem/TabItem";
 // import { SetAppData } from "../../store/action/Action";
-
+debugger;
 const TabHeader = (props) =>{
+    const [isSectorAvailable, setIsSectorAvailable] = useState(false);
     React.useEffect(() => {
-        
-    }, [props.sectors])
+        if(props.isSectorFetchComplete){
+            setIsSectorAvailable(true);
+        }
+    }, [props.isSectorFetchComplete])
     return(
         <div className={ styles.tabHeader }>
             {/* <UserContext.Provider value={...props}> */}
                 <h1>TabHeader (Organization, Department , Personal)</h1>
             {/* </UserContext.Provider> */}
           <div className={styles.SetDisplay}>
-            {props.sectors.map((t)=>{
+              {isSectorAvailable && 
+                 <label>{props.sectors.length}</label>
+             }
+            {/* {props.sectors.map((t)=>{
                 <TabItem {...t} />
-            })}
+            })} */}
             </div>
         </div> 
     )
