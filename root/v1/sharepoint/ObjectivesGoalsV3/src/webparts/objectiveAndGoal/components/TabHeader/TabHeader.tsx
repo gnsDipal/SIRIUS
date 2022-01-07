3c9914a64d47a117fb4d57f41cc27e3598518cc8
mangeshbhunde <69294754+mangeshbhunde@users.noreply.github.com>
2022-01-07 14:50:36 +0530

    UI Changes in TabHeader and TabItem file
    UI Changes in TabHeader and TabItem file

--- root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabHeader.tsx
+++ root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabHeader.tsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import styles from './TabHeader.module.scss';
 import { ITabHeaderProps } from './ITabHeaderProps';
 import { connect } from "react-redux";
+import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
 import TabItem from "./TabItem/TabItem";
 // import { SetAppData } from "../../store/action/Action";
 
@@ -13,13 +14,25 @@ const TabHeader = (props) =>{
     return(
         <div className={ styles.tabHeader }>
             {/* <UserContext.Provider value={...props}> */}
-                <h1>TabHeader (Organization, Department , Personal)</h1>
+            <h1>TabHeader (Organization, Department , Personal)</h1>
+            <div className={styles.TabHeaderSetDisplay}>                         
+                <div className={styles.TabHeaderGoalsTabs} >
+                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Organization </h3></DefaultButton>                       
+                </div>
+                <div className={styles.TabHeaderGoalsTabs} >
+                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Department </h3></DefaultButton>                       
+                </div>
+                <div className={styles.TabHeaderGoalsTabs} >
+                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Personal </h3></DefaultButton>                       
+                </div>
+            </div>
             {/* </UserContext.Provider> */}
-          <div className={styles.SetDisplay}>
+          {/* <div className={styles.SetDisplay}>
             {props.sectors.map((t)=>{
                 <TabItem {...t} />
             })}
-            </div>
+            </div> */}
+            <TabItem />
         </div> 
     )
 };
--- root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabItem/TabItem.tsx
+++ root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabItem/TabItem.tsx
@@ -4,7 +4,37 @@ import { ITabItemProps } from './ITabItemProps';
 import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
 
 const TabItem = (props) =>{
-return (<DefaultButton title={props.Title} id={props.Id} />); 
-};
+   const[ bgColorOrganization, setBgColorOrganization ] = React.useState<string>("#04ef00");
+   const[ colorOrganization,setColorOrganization ] = React.useState<string>("white");
+   const HeaderTabClicked = async() => {
+         alert("Header Tab Clicked");
+      };
+   return (
+      // <DefaultButton title={props.Title} id={props.Id} /> 
+      <div className={ styles.tabItem }>  
+      <h1>TabItem (Default Tab Item is Organization)</h1> 
+       {/* Header Tab related Information */}
+         <div className={styles.TabHeaderSetDisplay}>                         
+            <div className={styles.TabHeaderGoalsTabs} >
+               <DefaultButton className={styles.TabHeaderGoalsTabBtn} style={{backgroundColor:bgColorOrganization, color:colorOrganization}} onClick={HeaderTabClicked} ><h3> {props.Title} </h3></DefaultButton>                       
+            </div>
+         </div>
+      {/* Interval Data related Information */}
+       <div className={styles.IntervalSetDisplay}>   
+              <div className={styles.IntervalButtonDiv}><DefaultButton className={styles.IntervalButtons} ><h3>Monthly Target</h3></DefaultButton></div>                                                         
+              <div className={styles.DataDisplay}> 
+                <ul><li key="1" >                                                                
+                        <table><tr><td>{"Monthly Goal "}</td></tr>                            
+                               <tr><td><progress id="file" value="10" max="100"> </progress>10%                                                                
+                                  <button style={{margin:'20px'}} > Edit </button>                                 
+                               </td></tr>
+                        </table>                                               
+                    </li>                                                   
+                </ul>
+              </div> 
+        </div> 
+      </div>      
+   );
+ };
 
   export default TabItem
