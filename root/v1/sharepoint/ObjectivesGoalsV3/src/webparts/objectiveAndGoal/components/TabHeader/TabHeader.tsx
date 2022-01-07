e5cfb84efc4d1c243e85aa05f33c2cb61e881f28
mangeshbhunde <69294754+mangeshbhunde@users.noreply.github.com>
2022-01-07 19:06:55 +0530

    Display Static and Dynamic Header Data
    Display Static and Dynamic Header Data

--- root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabHeader.tsx
+++ root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/components/TabHeader/TabHeader.tsx
@@ -8,29 +8,48 @@ import TabItem from "./TabItem/TabItem";
 // import { SetAppData } from "../../store/action/Action";
 debugger;
 const TabHeader = (props) =>{
+    {console.log("props =>",props + "props.sectors =>",props.sectors )} 
     const [isSectorAvailable, setIsSectorAvailable] = useState(false);
     React.useEffect(() => {
         if(props.isSectorFetchComplete){
             setIsSectorAvailable(true);
         }
     }, [props.isSectorFetchComplete])
-    return(
+    return(       
         <div className={ styles.tabHeader }>
+           
             {/* <UserContext.Provider value={...props}> */}
-            <h1>TabHeader (Organization, Department , Personal)</h1>
-            <div className={styles.TabHeaderSetDisplay}>                         
+            <h1>TabHeader (Organization, Department , Personal)</h1>           
+           
+            {/* Added TabHeader name Dynamic */} 
+            { (isSectorAvailable) ?
+              // <h1> sectors Data Available and Its length : {props.sectors.length} </h1>              
+              props.sectors.map((t)=>{
+                return(
+                    <div className={styles.TabHeaderSetDisplay}>                         
+                    <div className={styles.TabHeaderGoalsTabs} >                  
+                            <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> {t.Title} </h3></DefaultButton>                                         
+                    </div>
+                    </div>
+                 )
+               })
+            :
+              <div className={styles.TabHeaderSetDisplay}>                                       
                 <div className={styles.TabHeaderGoalsTabs} >
-                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Organization </h3></DefaultButton>                       
+                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Tab -1 </h3></DefaultButton>                       
                 </div>
                 <div className={styles.TabHeaderGoalsTabs} >
-                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Department </h3></DefaultButton>                       
+                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Tab -2 </h3></DefaultButton>                       
                 </div>
                 <div className={styles.TabHeaderGoalsTabs} >
-                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Personal </h3></DefaultButton>                       
+                    <DefaultButton className={styles.TabHeaderGoalsTabBtn} ><h3> Tab -3 </h3></DefaultButton>                       
                 </div>
-            </div>
+             </div>
+
+            }
+           
             {/* </UserContext.Provider> */}
-          <div className={styles.TabHeaderSetDisplay}>
+            <div className={styles.TabHeaderSetDisplay}>
               {isSectorAvailable && 
                 props.sectors.map((t)=>{
                     <TabItem {...t} />
