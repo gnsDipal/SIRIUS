6f566bf1fa16f6659681742d57d629a2780bdf8c
Sourabh <77320234+SourabhMK@users.noreply.github.com>
2022-02-18 12:02:55 +0530

    debugger clean-up changes
    debugger clean-up changes

--- root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/store/action/sectors/setSPContext.ts
+++ root/v1/sharepoint/ObjectivesGoalsV3/src/webparts/objectiveAndGoal/store/action/sectors/setSPContext.ts
@@ -2,9 +2,11 @@ import type { SPContext } from '../../operations/types';
 import type { SetSPContext } from '../../operations/actionTypes';
 
 
-const setSPContext = (context: SPContext) : SetSPContext => ({
-    type: 'SET_SPCONTEXT',
-    payload: context
-});
+const setSPContext = (context: SPContext) : SetSPContext => (
+    {
+        type: 'SET_SPCONTEXT',
+        payload: context
+    }
+);
 
 export default setSPContext;
